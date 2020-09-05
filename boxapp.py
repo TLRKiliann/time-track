07ddce6e3db571c638f1a62a6194b7d1ff335496
TLRKiliann <philogenie@protonmail.com>
2020-09-05 10:45:09 +0200

    reconfig interface

--- boxapp.py
+++ boxapp.py
@@ -11,12 +11,16 @@ import datetime
 # Synopsis page
 def callBox(self):
     self.can.delete(ALL)
-    self.photo=PhotoImage(file='./syno_gif/fondcolor2.png')
-    self.item=self.can.create_image(625, 400, image=self.photo)
+    #self.photo=PhotoImage(file='./syno_gif/fondcolor2.png')
+    #self.item=self.can.create_image(625, 400, image=self.photo)
     #self.can.create_text(625, 80, anchor=CENTER, text="SYNOPSIS",
     #    font=('Times New Roman', 40), fill='turquoise')
 
-    self.x1, self.y1 = 1100, 50
+    # To backup à revoir !!!!!!!!!!!!!!!!!!!!!!!!
+    self.updateFiletxt()
+
+    # Display date
+    self.x1, self.y1 = 1065, 70
     self.Date_write=Entry(self.can)
     self.data_time=StringVar()
     self.Date_write=Entry(textvariable=self.data_time, width=10,
@@ -25,11 +29,8 @@ def callBox(self):
     self.Date_write=self.can.create_window(self.x1, self.y1,
         window=self.Date_write)
 
-    # To backup
-    self.updateFiletxt()
-
     # Static time
-    self.x2, self.y2 = 1100, 100
+    self.x2, self.y2 = 1165, 70
     self.Date_write2 = Entry(self.can)
     self.data_time2 = StringVar()
     self.Date_write2 = Entry(width=10, textvariable=self.data_time2,
@@ -40,36 +41,36 @@ def callBox(self):
     # To display time dynamically
 
     # To introduce a new pytient
-    self.x100, self.y100 = 130, 50
-    self.b100=Button(self.can, width=10, font=16, bd=3, highlightbackground='#82193e',
+    self.x100, self.y100 = 135, 110 # here
+    self.b100=Button(self.can, width=10, font=16, bd=3, highlightbackground='blue',
         bg='RoyalBlue3', fg='white', activebackground='dark turquoise',
         text="New Entry", command=self.callPatient1)
     self.fb100=self.can.create_window(self.x100, self.y100, window=self.b100)
+
+    # To add one patient and files
+    self.x200, self.y200 = 135, 170
+    self.b200=Button(self.can, width=10, font=16, bd=3, highlightbackground='blue',
+        bg='RoyalBlue3', fg='cyan', activebackground='dark turquoise', 
+        text="Add patient", command=self.addPatientAfter)
+    self.fb200=self.can.create_window(self.x200, self.y200, window=self.b200)
     
     # To refresh canvas + menu bar
-    self.x101, self.y101 = 270, 50
-    self.b101=Button(self.can, width=10, font=16, bd=3, highlightbackground='#82193e',
+    self.x101, self.y101 = 135, 230
+    self.b101=Button(self.can, width=10, font=16, bd=3, highlightbackground='blue',
         bg='RoyalBlue3', fg='SpringGreen2', activebackground='yellow', activeforeground='blue',
         text="Refresh", command=self.upDateAll)
     self.fb101=self.can.create_window(self.x101, self.y101, window=self.b101)
 
     # To delete one patient and all files
-    self.x200, self.y200 = 130, 100
-    self.b200=Button(self.can, width=10, font=16, bd=3, highlightbackground='#82193e',
+    self.x200, self.y200 = 135, 290
+    self.b200=Button(self.can, width=10, font=16, bd=3, highlightbackground='blue',
         bg='RoyalBlue3', fg='coral', activebackground='black', activeforeground='red',
         text="Delete patient", command=self.delEverPat)
     self.fb200=self.can.create_window(self.x200, self.y200, window=self.b200)
 
-    # To add one patient and files
-    self.x200, self.y200 = 270, 100
-    self.b200=Button(self.can, width=10, font=16, bd=3, highlightbackground='#82193e',
-        bg='RoyalBlue3', fg='cyan', activebackground='dark turquoise', 
-        text="Add patient", command=self.addPatientAfter)
-    self.fb200=self.can.create_window(self.x200, self.y200, window=self.b200)
-
     # TextBox !!!!!!!!!!!!!!!!!!!!!!!!!!!!@@@@@@########################
-    self.x63, self.y63 = 625, 325
-    self.t63=Text(self.can, height=15, width=60, font=18, relief=SUNKEN)
+    self.x63, self.y63 = 625, 200
+    self.t63=Text(self.can, height=15, width=70, font=18, relief=SUNKEN)
     self.t63.insert(INSERT, "Previously (yesterday last infos) : ")
     self.t63.insert(END, (datetime.datetime.now() + datetime.timedelta(days=-1)).strftime('%d/%m/%Y'))
         #time.strftime("%d/%m/%Y at %H:%M:%S :\n"))
@@ -332,16 +333,8 @@ def callBox(self):
     except FileNotFoundError as callfile:
         print("File entryfile.txt doesn't exist !", callfile)
 
-    #Patient1
-    # For label below (in me2.add_command)
-    try:
-        with open('./newpatient/entryfile.txt', 'r') as namefile:
-            line1=namefile.readline()
-    except FileNotFoundError as callfile:
-        print("File entryfile.txt doesn't exist !", callfile)
-
     self.data_time=line1
-    self.x2, self.y2 = 129, 525
+    self.x2, self.y2 = 129, 400
     self.Data_write=Entry(self.can)
     self.new_data1=StringVar()
     self.Data_write=Entry(textvariable=self.new_data1,
@@ -350,19 +343,19 @@ def callBox(self):
     self.Data_write=self.can.create_window(self.x2, self.y2,
         window=self.Data_write)
 
-    self.x3, self.y3 = 271, 525
+    self.x3, self.y3 = 271, 400
     self.b=Button(self.can, width=8, font=16, bg='grey30', fg='coral',
         activebackground='dark turquoise', text="Allergy",
         command=self.allergyLink)
     self.fb=self.can.create_window(self.x3, self.y3, window=self.b)
 
-    self.x3, self.y3 = 429, 525
+    self.x3, self.y3 = 429, 400
     self.b=Button(self.can, width=18, font=16, bg='grey30', fg='cyan',
         activebackground='dark turquoise', text="Diagnostic + ATCD",
         command=self.diag1)
     self.fb=self.can.create_window(self.x3, self.y3, window=self.b)
 
-    self.x4, self.y4 = 597, 525
+    self.x4, self.y4 = 597, 400
     self.b4=Button(self.can, width=10, font=16, bg='grey30', fg='cyan',
         activebackground='dark turquoise', text="Laboratory",
         command=self.laboResult)
@@ -376,7 +369,7 @@ def callBox(self):
         print("File entryfile2.txt doesn't exist !", callfile2)
 
     self.new_data2=line2
-    self.x9, self.y9 = 129, 557
+    self.x9, self.y9 = 129, 432
     self.Data_write=Entry(self.can)
     self.new_data2=StringVar()
     self.Data_write=Entry(textvariable=self.new_data2,
@@ -385,19 +378,19 @@ def callBox(self):
     self.Data_write=self.can.create_window(self.x9, self.y9,
       window=self.Data_write)
 
-    self.x10, self.y10 = 271, 557
+    self.x10, self.y10 = 271, 432
     self.b10=Button(self.can, width=8, font=16, bg='grey25', fg='coral',
         activebackground='dark turquoise', text="Allergy",
         command=self.allergyLink2)
     self.fb10=self.can.create_window(self.x10, self.y10, window=self.b10)
 
-    self.x13, self.y13 = 429, 557
+    self.x13, self.y13 = 429, 432
     self.b13=Button(self.can, width=18, font=16, bg='grey25', fg='cyan',
         activebackground='dark turquoise', text="Diagnostic + ATCD",
         command=self.diag2)
     self.fb13=self.can.create_window(self.x13, self.y13, window=self.b13)
 
-    self.x14, self.y14 = 597, 557
+    self.x14, self.y14 = 597, 432
     self.b14=Button(self.can, width=10, font=16, bg='grey25', fg='cyan',
         activebackground='dark turquoise', text="Laboratory",
         command=self.laboResult2)
@@ -411,7 +404,7 @@ def callBox(self):
         print("File entryfile3.txt doesn't exist !", callfile3)
 
     self.new_data3=line3
-    self.x18, self.y18 = 129, 589
+    self.x18, self.y18 = 129, 464
     self.Data_write=Entry(self.can)
     self.new_data3=StringVar()
     self.Data_write=Entry(textvariable=self.new_data3,
@@ -420,19 +413,19 @@ def callBox(self):
     self.Data_write=self.can.create_window(self.x18, self.y18,
       window=self.Data_write)
 
-    self.x19, self.y19 = 271, 589
+    self.x19, self.y19 = 271, 464
     self.b19=Button(self.can, width=8, font=16, bg='grey20', fg='coral',
         activebackground='dark turquoise', text="Allergy",
         command=self.allergyLink3)
     self.fb19=self.can.create_window(self.x19, self.y19, window=self.b19)
 
-    self.x22, self.y22 = 429, 589
+    self.x22, self.y22 = 429, 464
     self.b22=Button(self.can, width=18, font=16, bg='grey20', fg='cyan',
         activebackground='dark turquoise', text="Diagnostic + ATCD",
         command=self.diag3)
     self.fb22=self.can.create_window(self.x22, self.y22, window=self.b22)
 
-    self.x23, self.y23 = 597, 589
+    self.x23, self.y23 = 597, 464
     self.b23=Button(self.can, width=10, font=16, bg='grey20', fg='cyan',
         activebackground='dark turquoise', text="Laboratory",
         command=self.laboResult3)
@@ -446,7 +439,7 @@ def callBox(self):
         print("File entryfile4.txt doesn't exist !", callfile4)
 
     self.new_data4=line4
-    self.x27, self.y27 = 129, 621
+    self.x27, self.y27 = 129, 496
     self.Data_write=Entry(self.can)
     self.new_data4=StringVar()
     self.Data_write=Entry(textvariable=self.new_data4,
@@ -455,19 +448,19 @@ def callBox(self):
     self.Data_write=self.can.create_window(self.x27, self.y27,
       window=self.Data_write)
 
-    self.x28, self.y28 = 271, 621
+    self.x28, self.y28 = 271, 496
     self.b28=Button(self.can, width=8, font=16, bg='grey18', fg='coral',
         activebackground='dark turquoise', text="Allergy",
         command=self.allergyLink4)
     self.fb28=self.can.create_window(self.x28, self.y28, window=self.b28)
 
-    self.x31, self.y31 = 429, 621
+    self.x31, self.y31 = 429, 496
     self.b31=Button(self.can, width=18, font=16, bg='grey18', fg='cyan',
         activebackground='dark turquoise', text="Diagnostic + ATCD",
         command=self.diag4)
     self.fb31=self.can.create_window(self.x31, self.y31, window=self.b31)
 
-    self.x32, self.y32 = 597, 621
+    self.x32, self.y32 = 597, 496
     self.b32=Button(self.can, width=10, font=16, bg='grey18', fg='cyan',
         activebackground='dark turquoise', text="Laboratory",
         command=self.laboResult4)
@@ -481,7 +474,7 @@ def callBox(self):
         print("File entryfile5.txt doesn't exist !", callfile5)
 
     self.new_data5=line5
-    self.x36, self.y36 = 129, 653
+    self.x36, self.y36 = 129, 528
     self.Data_write=Entry(self.can)
     self.new_data5=StringVar()
     self.Data_write=Entry(textvariable=self.new_data5,
@@ -490,19 +483,19 @@ def callBox(self):
     self.Data_write=self.can.create_window(self.x36, self.y36,
       window=self.Data_write)
 
-    self.x37, self.y37 = 271, 653
+    self.x37, self.y37 = 271, 528
     self.b37=Button(self.can, width=8, font=16, bg='grey15', fg='coral',
         activebackground='dark turquoise', text="Allergy",
         command=self.allergyLink5)
     self.fb37=self.can.create_window(self.x37, self.y37, window=self.b37)
 
-    self.x40, self.y40 = 429, 653
+    self.x40, self.y40 = 429, 528
     self.b40=Button(self.can, width=18, font=16, bg='grey15', fg='cyan',
         activebackground='dark turquoise', text="Diagnostic + ATCD",
         command=self.diag5)
     self.fb40=self.can.create_window(self.x40, self.y40, window=self.b40)
 
-    self.x41, self.y41 = 597, 653
+    self.x41, self.y41 = 597, 528
     self.b41=Button(self.can, width=10, font=16, bg='grey15', fg='cyan',
         activebackground='dark turquoise', text="Laboratory",
         command=self.laboResult5)
@@ -516,7 +509,7 @@ def callBox(self):
         print("File entryfile6.txt doesn't exist !", callfile6)
 
     self.new_data6=line6
-    self.x45, self.y45 = 129, 685
+    self.x45, self.y45 = 129, 560
     self.Data_write=Entry(self.can)
     self.new_data6=StringVar()
     self.Data_write=Entry(textvariable=self.new_data6,
@@ -525,19 +518,19 @@ def callBox(self):
     self.Data_write=self.can.create_window(self.x45, self.y45,
       window=self.Data_write)
 
-    self.x46, self.y46 = 271, 685
+    self.x46, self.y46 = 271, 560
     self.b46=Button(self.can, width=8, font=16, bg='grey12', fg='coral',
         activebackground='dark turquoise', text="Allergy",
         command=self.allergyLink6)
     self.fb46=self.can.create_window(self.x46, self.y46, window=self.b46)
 
-    self.x49, self.y49 = 429, 685
+    self.x49, self.y49 = 429, 560
     self.b49=Button(self.can, width=18, font=16, bg='grey12', fg='cyan',
         activebackground='dark turquoise', text="Diagnostic + ATCD",
         command=self.diag6)
     self.fb49=self.can.create_window(self.x49, self.y49, window=self.b49)
 
-    self.x50, self.y50 = 597, 685
+    self.x50, self.y50 = 597, 560
     self.b50=Button(self.can, width=10, font=16, bg='grey12', fg='cyan',
         activebackground='dark turquoise', text="Laboratory",
         command=self.laboResult6)
@@ -551,7 +544,7 @@ def callBox(self):
         print("File entryfile7.txt doesn't exist !", callfile7)
 
     self.new_data7=line7
-    self.x54, self.y54 = 129, 717
+    self.x54, self.y54 = 129, 592
     self.Data_write=Entry(self.can)
     self.new_data7=StringVar()
     self.Data_write=Entry(textvariable=self.new_data7,
@@ -560,19 +553,19 @@ def callBox(self):
     self.Data_write=self.can.create_window(self.x54, self.y54,
       window=self.Data_write)
 
-    self.x54, self.y54 = 271, 717
+    self.x54, self.y54 = 271, 592
     self.b54=Button(self.can, width=8, font=16, bg='black', fg='coral',
         activebackground='dark turquoise', text="Allergy",
         command=self.allergyLink7)
     self.fb54=self.can.create_window(self.x54, self.y54, window=self.b54)
 
-    self.x57, self.y57 = 429, 717
+    self.x57, self.y57 = 429, 592
     self.b57=Button(self.can, width=18, font=16, bg='black', fg='cyan',
         activebackground='dark turquoise', text="Diagnostic + ATCD",
         command=self.diag7)
     self.fb57=self.can.create_window(self.x57, self.y57, window=self.b57)
 
-    self.x58, self.y58 = 597, 717
+    self.x58, self.y58 = 597, 592
     self.b58=Button(self.can, width=10, font=16, bg='black', fg='cyan',
         activebackground='dark turquoise', text="Laboratory",
         command=self.laboResult7)
@@ -586,7 +579,7 @@ def callBox(self):
         print("File entryfile8.txt doesn't exist !", callfile8)
 
     self.new_data8=line8
-    self.x54, self.y54 = 129, 749
+    self.x54, self.y54 = 129, 624
     self.Data_write=Entry(self.can)
     self.new_data8=StringVar()
     self.Data_write=Entry(textvariable=self.new_data8,
@@ -595,19 +588,19 @@ def callBox(self):
     self.Data_write=self.can.create_window(self.x54, self.y54,
       window=self.Data_write)
 
-    self.x54, self.y54 = 271, 749
+    self.x54, self.y54 = 271, 624
     self.b54=Button(self.can, width=8, font=16, bg='black', fg='coral',
         activebackground='dark turquoise', text="Allergy",
         command=self.allergyLink8)
     self.fb54=self.can.create_window(self.x54, self.y54, window=self.b54)
 
-    self.x57, self.y57 = 429, 749
+    self.x57, self.y57 = 429, 624
     self.b57=Button(self.can, width=18, font=16, bg='black', fg='cyan',
         activebackground='dark turquoise', text="Diagnostic + ATCD",
         command=self.diag8)
     self.fb57=self.can.create_window(self.x57, self.y57, window=self.b57)
 
-    self.x58, self.y58 = 597, 749
+    self.x58, self.y58 = 597, 624
     self.b58=Button(self.can, width=10, font=16, bg='black', fg='cyan',
         activebackground='dark turquoise', text="Laboratory",
         command=self.laboResult8)
@@ -621,7 +614,7 @@ def callBox(self):
         print("File entryfile9.txt doesn't exist !", callfile9)
 
     self.new_data9=line9
-    self.x54, self.y54 = 129, 781
+    self.x54, self.y54 = 129, 656
     self.Data_write=Entry(self.can)
     self.new_data9=StringVar()
     self.Data_write=Entry(textvariable=self.new_data9,
@@ -630,19 +623,19 @@ def callBox(self):
     self.Data_write=self.can.create_window(self.x54, self.y54,
       window=self.Data_write)
 
-    self.x54, self.y54 = 271, 781
+    self.x54, self.y54 = 271, 656
     self.b54=Button(self.can, width=8, font=16, bg='black', fg='coral',
         activebackground='dark turquoise', text="Allergy",
         command=self.allergyLink9)
     self.fb54=self.can.create_window(self.x54, self.y54, window=self.b54)
 
-    self.x57, self.y57 = 429, 781
+    self.x57, self.y57 = 429, 656
     self.b57=Button(self.can, width=18, font=16, bg='black', fg='cyan',
         activebackground='dark turquoise', text="Diagnostic + ATCD",
         command=self.diag9)
     self.fb57=self.can.create_window(self.x57, self.y57, window=self.b57)
 
-    self.x58, self.y58 = 597, 781
+    self.x58, self.y58 = 597, 656
     self.b58=Button(self.can, width=10, font=16, bg='black', fg='cyan',
         activebackground='dark turquoise', text="Laboratory",
         command=self.laboResult9)
@@ -656,7 +649,7 @@ def callBox(self):
         print("File entryfile10.txt doesn't exist !", callfile10)
 
     self.new_data10=line10
-    self.x54, self.y54 = 129, 813
+    self.x54, self.y54 = 129, 688
     self.Data_write=Entry(self.can)
     self.new_data10=StringVar()
     self.Data_write=Entry(textvariable=self.new_data10,
@@ -665,19 +658,19 @@ def callBox(self):
     self.Data_write=self.can.create_window(self.x54, self.y54,
       window=self.Data_write)
 
-    self.x54, self.y54 = 271, 813
+    self.x54, self.y54 = 271, 688
     self.b54=Button(self.can, width=8, font=16, bg='black', fg='coral',
         activebackground='dark turquoise', text="Allergy",
         command=self.allergyLink10)
     self.fb54=self.can.create_window(self.x54, self.y54, window=self.b54)
 
-    self.x57, self.y57 = 429, 813
+    self.x57, self.y57 = 429, 688
     self.b57=Button(self.can, width=18, font=16, bg='black', fg='cyan',
         activebackground='dark turquoise', text="Diagnostic + ATCD",
         command=self.diag10)
     self.fb57=self.can.create_window(self.x57, self.y57, window=self.b57)
 
-    self.x58, self.y58 = 597, 813
+    self.x58, self.y58 = 597, 688
     self.b58=Button(self.can, width=10, font=16, bg='black', fg='cyan',
         activebackground='dark turquoise', text="Laboratory",
         command=self.laboResult10)
