cc04409636836a3d1cb2dffd593bafa2f8e8b45f
TLRKiliann <philogenie@protonmail.com>
2020-09-05 18:31:54 +0200

    diag complete with 24 !

---
+++ diag/diag_patient20.py
@@ -0,0 +1,119 @@
+#!/usr/bin/python3
+#!-*-encoding:Utf-8-*-
+
+
+from tkinter import *
+from tkinter import messagebox
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import subprocess
+import os
+
+
+# La ScrollBar en class! Préparation pour l'application.
+class ScrollCanvas(Frame):
+    def __init__(self, boss=None):
+        Frame.__init__(self, borderwidth=borderwidth, relief=relief)
+        self.can=Canvas(self, width=width, height=height, bd=bd, bg=bg,
+            relief=relief)
+        self.frame = Frame(self.can)
+
+        self.vsb = Scrollbar(self, orient=VERTICAL, command=self.can.yview)
+        self.can.configure(yscrollcommand=self.vsb.set)
+
+        self.vsb.pack(side=RIGHT, fill=Y)
+        self.can.pack(side=LEFT, fill=BOTH, expand=YES)
+        self.can.create_window((4,4), window=self.frame, anchor=NW, 
+                                  tags="self.frame")
+        self.frame.bind("<Configure>", self.onFrameConfigure)
+
+# Class de la barre des menus
+class MenuBar(Frame):
+    """Barre menu déroulant"""
+    def __init__(self, boss=None):
+        Frame.__init__(self, borderwidth=5, bg='dim gray', padx=0)
+        # Menu fichier
+        But=Button(self, text ="Close", fg='cyan', bg='navy',
+            activebackground='cyan', command=boss.quit).pack(side=LEFT,
+            padx=3)
+
+# Application principale
+class Application(Frame):
+    def __init__(self, boss=None):
+        Frame.__init__(self)
+        self.master.title('ANGEL-VISION - Developed by CK - Dec. 2018')
+        mBar=MenuBar(self)
+        mBar.pack(side=TOP, fill=X, expand=1)
+        # ScrollCanvas limite de la zone à parcourir avec la barre
+        self.can=Canvas(self, width=600, height=400, bg='#82193e')
+        self.frame = Frame(self.can)
+        self.vsb = Scrollbar(self, orient=VERTICAL, command=self.can.yview)
+        self.can.configure(yscrollcommand=self.vsb.set)
+        self.vsb.pack(side=RIGHT, fill=Y)
+        self.can.pack(side=LEFT, fill=BOTH, expand=YES)
+        self.can.create_window((4,4), window=self.frame, anchor=NW, 
+                                  tags="self.frame")
+        # Insertion du texte
+        self.can.create_text(300, 140, anchor=CENTER, text="Diagnostics and ATCD",
+                    font=('Times New Roman', 28), fill='cyan')
+        self.can.create_text(590, 380, anchor=NE, text="ANGEL-VISION",
+                    font=('Times', 12), fill='white') 
+        self.can.pack(side=LEFT, fill=BOTH, expand=1)
+        # Configuration de la Scrollbar sur le Frame
+        self.frame.bind("<Configure>", self.onFrameConfigure)
+        # Création des boutons
+        self.x2, self.y2 = 200, 250
+        self.b2=Button(self.can, width=10, font=16, bg='RoyalBlue3', fg='gold',
+            activebackground='dark turquoise', bd=3, highlightbackground='grey17', 
+            text="Add", command=self.Frame_Ap1)
+        self.fb2=self.can.create_window(self.x2, self.y2, window=self.b2)
+
+        self.x3, self.y3 = 400, 250
+        self.b3=Button(self.can, width=10, font=16, bg='RoyalBlue3', fg='gold',
+            activebackground='dark turquoise', bd=3, highlightbackground='grey17', 
+            text="Read", command=self.Frame_Ap2)
+        self.fb3=self.can.create_window(self.x3, self.y3, window=self.b3)
+        self.pack()
+
+    # Méthode pour reconfigurer la scrollbar à chaque fois
+    def onFrameConfigure(self, event):
+        '''Reset the scroll region to encompass the inner frame'''
+        self.can.configure(scrollregion=self.can.bbox(ALL))
+
+    def Frame_Ap1(self):
+        """
+        To verify and write in diag file
+        """
+        try:
+            if os.path.getsize('./diag/doc_diag20/diagrecap20.txt'):
+                print("+ File 'Diag' exist (add)!")
+                subprocess.call('./diag/doc_diag20/diag_write.py')
+        except FileNotFoundError as outmsg:
+            print("+ Sorry, file 'Diag' not exist !", outmsg)
+            print("+ File diag.txt created !")
+            with open('./diag/doc_diag20/diagrecap20.txt', 'w') as file:
+                file.write(".")
+            self.confRec()
+            subprocess.call('./diag/doc_diag20/diag_write.py')
+
+    def Frame_Ap2(self):
+        """
+        To verify and read diag file
+        """
+        try:
+            if os.path.getsize('./diag/doc_diag20/diagrecap20.txt'):
+                print("+ File 'Diag' exist (read)!")
+                subprocess.call('./diag/doc_diag20/diag_read.py')
+        except FileNotFoundError as outcom:
+            print("+ Sorry, file 'Diag' not exist !", outcom)
+            self.confRec()
+
+    def confRec(self):
+        self.MsgBox2msg = messagebox.showinfo("Warning", "File 'Diag'"
+            "was created, but no Diagnosis has been checked !")
+
+if __name__=='__main__':
+    app = Application()
+    app.mainloop()
+    
---
+++ diag/doc_diag12/diag_write.py
@@ -0,0 +1,115 @@
+#!/usr/bin/python3
+# -*-encoding:Utf-8-*-
+
+
+from tkinter import *
+import subprocess
+import time
+from tkinter import messagebox
+
+
+def retrieve_input():
+    file = open('./diag/doc_diag12/diagrecap12.txt', 'a+')
+    file.write(textBox.get("1.0", "end-1c"))
+    file.write(str('\n\n'))
+    file.close()
+
+def messFromSafeButt():
+    MsgBox = messagebox.askquestion("Confirm","Are you sure ?\n"
+        "It will save all data !")
+    if MsgBox == 'yes':
+        retrieve_input()
+        textBox.insert(INSERT, "\n---Data saved !---")
+        print("+ Data saved !")
+    else:
+        textBox.insert(INSERT, "Nothing has been saved !")
+        print("+ Nothing has been saved !")
+
+def lectureFic():
+    file = open('./diag/doc_diag12/diagrecap12.txt', 'r')
+    print(file.read())
+    file.close()
+    subprocess.call('./diag/doc_diag12/diag_read.py')
+
+def ajouterText():
+    textBox.delete('1.0', END)
+    textBox.insert(INSERT, "En date du : ")
+    textBox.insert(END, time.strftime("%d/%m/%Y à %H:%M:%S :") + '\n')
+    textBox.update()
+
+def importationFile(fichier, encodage="Utf-8"):
+    file = open(fichier, 'r', encoding=encodage)
+    content=file.readlines()
+    file.close()
+    for li in content:
+        textBox.insert(END, li)
+
+root=Tk()
+root.title("Diagnostics and ATCD")
+root.configure(background='#82193e')
+
+# To place side by side labelo + entrylab
+top = Frame(root, bg='#82193e')
+bottom = Frame(root, bg='#82193e')
+top.pack(side=TOP)
+bottom.pack(side=BOTTOM, fill=BOTH, expand=YES)
+
+labelo=Label(root, text="Diagnostics and ATCD for : ",
+    font='Arial 18 bold', fg='cyan', bg='#82193e')
+labelo.pack(in_=top, side=LEFT, padx=5, pady=20)
+
+with open('./newpatient/entryfile12.txt', 'r') as filename:
+    line1=filename.readline()
+
+textname=StringVar()
+entryName=Entry(root, textvariable=textname)
+textname.set(line1)
+entryName.pack(in_=top, side=LEFT, padx=10, pady=20)
+
+labelallergy=Label(root, text="Allergy",
+    font='Arial 18 bold', fg='coral', bg='#82193e')
+labelallergy.pack(padx=5, pady=10)
+
+with open('./allergy/allergyfile12.txt', 'r') as filename:
+    lineA1=filename.readline()
+    lineA2=filename.readline()
+    lineA3=filename.readline()
+    lineA4=filename.readline()
+    lineA5=filename.readline()
+    lineA6=filename.readline()
+    lineA7=filename.readline()
+
+entrytext=StringVar()
+entrytext.set(lineA1 + ', ' + lineA3 + ', ' + lineA5 + ', ' + lineA7)
+entryName=Entry(root, textvariable=entrytext, width=60)
+entryName.pack(padx=10, pady=10)
+
+textBox=Text(root, height=15, width=60, font=18, relief=SUNKEN)
+textBox.insert(INSERT, "\nEn date du : ")
+textBox.insert(END, time.strftime("%d/%m/%Y à %H:%M:%S :\n"))
+textBox.pack(padx=30, pady=30)
+
+buttonLire=Button(root, text="Read", fg='cyan', bg='navy',
+    activebackground='dark turquoise', activeforeground='navy',
+    bd=3, highlightbackground='grey17', command=lectureFic)
+buttonLire.pack(side='left', padx=10, pady=10)
+
+buttonAjouter=Button(root, text="1-Add", fg='yellow', bg='navy',
+    activebackground='dark turquoise', activeforeground='navy',
+    bd=3, highlightbackground='grey17', command=ajouterText)
+buttonAjouter.pack(side='left', padx=10, pady=10)
+
+buttonEnter=Button(root, text="2-Save", fg='yellow', bg='navy',
+    activebackground='dark turquoise', activeforeground='navy',
+    bd=3, highlightbackground='grey17', command=messFromSafeButt)
+buttonEnter.pack(side='left', padx=10, pady=10)
+
+buttonQuitter=Button(root, text="Quit", fg='white', bg='navy',
+    width=10, activebackground='cyan', activeforeground='navy',
+    bd=3, highlightbackground='grey17', command=quit)
+buttonQuitter.pack(side='right', padx=10, pady=10)
+
+importationFile('./diag/doc_diag12/diagrecap12.txt',
+    encodage="Utf-8")
+
+mainloop()
