9fcc9389d7f3a8bf219bbc004612dbe1a78d0a46
TLRKiliann <philogenie@protonmail.com>
2020-10-04 22:23:31 +0200

    date format '/' changed with '-' str

--- calBmi/doc_BMI24/convert_kg.py
+++ calBmi/doc_BMI24/convert_kg.py
@@ -89,8 +89,8 @@ with plt.style.context('dark_background'):
     ax = plt.gcf().axes[0]
     ax.xaxis.set_major_formatter(formatter)
     #axes.xaxis.set_major_formatter(AutoDateFormatter(locator))
-    min_date = date2num(datetime.datetime.strptime('01-01-2020', "%d-%m-%Y"))
-    max_date = date2num(datetime.datetime.strptime('31-12-2020', "%d-%m-%Y"))
+    min_date = date2num(datetime.datetime.strptime('01/01/2020', "%d-%m-%Y"))
+    max_date = date2num(datetime.datetime.strptime('31/12/2020', "%d-%m-%Y"))
     axes.set_xlim([min_date, max_date])
     #figure.autofmt_xdate()
 
