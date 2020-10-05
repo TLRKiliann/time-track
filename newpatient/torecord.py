1df4a0493f9d45cd1c69e48ab551de8fd04cfdbf
TLRKiliann <philogenie@protonmail.com>
2020-10-05 12:43:23 +0200

    tested with pylint

--- newpatient/torecord.py
+++ newpatient/torecord.py
@@ -2,6 +2,12 @@
 # -*-encoding:Utf-8-*-
 
 
+"""
+    To record a new patient
+    in text zone (entree cap)
+"""
+
+
 from tkinter import *
 from tkinter import messagebox
 import os
@@ -14,8 +20,8 @@ gui.configure(bg='cyan')
 
 def get(Nompatient, entree, Birthvalue, Birth_entree):
     """
-    Add a patient with
-    add button
+        Add a patient with
+        the add button
     """
     mot = "-"
     mot2 = "--"
@@ -314,6 +320,11 @@ def get(Nompatient, entree, Birthvalue, Birth_entree):
     gui.destroy()
 
 def searchLine1(Nompatient, Birthvalue):
+    """
+        To save new patient name
+        by a msgbox and write the
+        new name in an entryfile.txt
+    """
     MsgBox = messagebox.askyesno('Save data', 'Do you want to save ?')
     if MsgBox == 1:
         file = open('./newpatient/entryfile.txt', 'w')
@@ -483,16 +494,15 @@ def searchLine24(Nompatient, Birthvalue):
             file.write(Nompatient + '\n')
             file.write(Birthvalue + '\n')
 
-
 labelName = Label(gui)
-labelName = Label(text='Enter NAME : ', 
-    font="Times 14 bold", 
+labelName = Label(text='Enter NAME : ',
+    font="Times 14 bold",
     fg='RoyalBlue4', bg='cyan')
 labelName.pack(pady=10)
 
 Nompatient=StringVar()
 Nompatient.set('Firstname + Lastname')
-entree = Entry(gui, textvariable=Nompatient, 
+entree = Entry(gui, textvariable=Nompatient,
     highlightbackground='light sky blue', bd=4)
 entree.pack()
 
@@ -503,20 +513,20 @@ labelBirth.pack(pady=10)
 
 Birthvalue=StringVar()
 Birthvalue.set('Format: 00/00/0000')
-Birth_entree = Entry(gui, textvariable=Birthvalue, 
-    highlightbackground='light sky blue', 
+Birth_entree = Entry(gui, textvariable=Birthvalue,
+    highlightbackground='light sky blue',
     bd=4)
 Birth_entree.pack()
 
 bouton1 = Button(gui, text="Enter", width=8, bd=4,
-    fg='yellow', bg='RoyalBlue3', 
+    fg='yellow', bg='RoyalBlue3',
     highlightbackground='light sky blue',
     activebackground='dark turquoise',
     command = lambda: get(Nompatient, entree, Birthvalue, Birth_entree))
 bouton1.pack(side=LEFT, padx=10, pady=20)
 
 buttQuit=Button(gui, text="Quit", width=8, bd=4,
-    fg='cyan', bg='RoyalBlue3', 
+    fg='cyan', bg='RoyalBlue3',
     highlightbackground='light sky blue',
     activebackground='dark turquoise', command=quit)
 buttQuit.pack(side=LEFT, padx=10, pady=20)
