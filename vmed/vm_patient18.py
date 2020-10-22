f1b77f4af67ee08776e6659a8b9ccde05f3ebfaa
TLRKiliann <philogenie@protonmail.com>
2020-10-22 19:42:07 +0200

    diag files and vmed files on work

--- vmed/vm_patient18.py
+++ vmed/vm_patient18.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# -*-encoding:Utf-8-*-
+# -*- coding: utf-8 -*-
 
 
 from tkinter import *
@@ -18,7 +18,7 @@ class ScrollCanvas(Frame):
 
         self.vsb = Scrollbar(self, orient=VERTICAL, command=self.can.yview)
         self.can.configure(yscrollcommand=self.vsb.set)
-
+        
         self.vsb.pack(side=RIGHT, fill=Y)
         self.can.pack(side=LEFT, fill=BOTH, expand=YES)
         self.can.create_window((4, 4), window=self.frame, anchor=NW,
@@ -57,54 +57,56 @@ class Application(Frame):
         self.can.pack(side=LEFT, fill=BOTH, expand=1)
         # Configuration de la Scrollbar sur le Frame
         self.frame.bind("<Configure>", self.onFrameConfigure)
-        
-        # Butrton to add
+        # Button to add
         self.x2, self.y2 = 200, 250
-        self.b2=Button(self.can, width=10, font=16, bg='RoyalBlue3', fg='yellow',
-            activebackground='dark turquoise', bd=3, highlightbackground='cyan', 
+        self.b2=Button(self.can, width=10, font=16, bg='RoyalBlue2', fg='white',
+            activebackground='aquamarine', bd=3, highlightbackground='cyan',
             text="Add", command=self.lienDirect)
         self.fb2=self.can.create_window(self.x2, self.y2, window=self.b2)
-
+        # Button to read
         self.x3, self.y3 = 400, 250
-        self.b3=Button(self.can, width=10, font=16, bg='RoyalBlue3', fg='yellow',
-            activebackground='dark turquoise', bd=3, highlightbackground='cyan', 
+        self.b3=Button(self.can, width=10, font=16, bg='RoyalBlue2', fg='white',
+            activebackground='aquamarine', bd=3, highlightbackground='cyan',
             text="Read", command=self.lectureFic)
         self.fb3=self.can.create_window(self.x3, self.y3, window=self.b3)
         self.pack()
 
-    # Méthode pour reconfigurer la scrollbar à chaque fois
+    # Method to reconfigure scrollbar everytime
     def onFrameConfigure(self, event):
         '''Reset the scroll region to encompass the inner frame'''
         self.can.configure(scrollregion=self.can.bbox(ALL))
 
-    # Func to add
     def lienDirect(self):
+        """
+        To verify and write in VM file
+        """
         try:
-            if os.path.getsize('./vmed/doc_vmed18/resultvmed.txt'):
-                print("+ File 'VMED18' exist (add)!")
+            if os.path.getsize('./vmed/doc_vmed18/resultvmed18.txt'):
+                print("+ File 'VMED' exist (add)!")
                 subprocess.run('./vmed/doc_vmed18/vmed_write.py', check=True)
         except FileNotFoundError as outmsg:
-            print("+ Sorry, file 'VMED18' not exist !", outmsg)
-            print("+ File VMED18 created !")
-            with open('./vmed/doc_vmed18/resultvmed.txt', 'w') as file:
-                file.write(".")
+            print("+ Sorry, file 'VMED' not exist !", outmsg)
+            print("+ File VMED created !!!")
+            with open('./vmed/doc_vmed18/resultvmed18.txt', 'w') as file:
+                file.write("--- MEDICAL VISIT ---\n")
             self.confRec()
 
-    # Func to read
     def lectureFic(self):
+        """
+        To verify and read diag file
+        """
         try:
-            if os.path.getsize('./vmed/doc_vmed18/resultvmed.txt'):
-                print("+ File 'VMED18' exist (read)!")
+            if os.path.getsize('./vmed/doc_vmed18/resultvmed18.txt'):
+                print("+ File 'VMED' exist (read)!")
                 subprocess.run('./vmed/doc_vmed18/vmed_read.py', check=True)
         except FileNotFoundError as outcom:
-            print("+ Sorry, file 'VMED18' not exist !", outcom)
-            with open('./vmed/doc_vmed18/resultvmed.txt', 'w') as file:
-                file.write(".")
+            print("+ Sorry, file 'VMED' not exist !", outcom)
             self.confRec()
 
     def confRec(self):
-        self.MsgBox2msg = messagebox.showinfo("Warning", "File 'VMED'"
+        self.MsgBox2msg = messagebox.showinfo("Warning", "File 'VMED' "
             "was created, but no Medical Visit has been checked !")
+        subprocess.run('./vmed/doc_vmed18/vmed_write.py', check=True)
 
 if __name__=='__main__':
     app = Application()
