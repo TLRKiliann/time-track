9bc5de3fddfada3c28313a9e06f086c465ca8ec1
TLRKiliann <philogenie@protonmail.com>
2020-10-22 23:43:36 +0200

    diag files and vmed files on work

--- diag/doc_diag17/diag_write.py
+++ diag/doc_diag17/diag_write.py
@@ -1,48 +1,13 @@
 #!/usr/bin/python3
-# -*-encoding:Utf-8-*-
+# -*- coding: utf-8 -*-
 
 
 from tkinter import *
-import subprocess
-import time
 from tkinter import messagebox
+import time
+import os
+import subprocess
 
-
-def retrieve_input():
-    file = open('./diag/doc_diag17/diagrecap17.txt', 'a+')
-    file.write(textBox.get("1.0", "end-1c"))
-    file.write(str('\n\n'))
-    file.close()
-
-def messFromSafeButt():
-    MsgBox = messagebox.askquestion("Confirm","Are you sure ?\n"
-        "It will save all data !")
-    if MsgBox == 'yes':
-        retrieve_input()
-        textBox.insert(INSERT, "\n---Data saved !---")
-        print("+ Data saved !")
-    else:
-        textBox.insert(INSERT, "Nothing has been saved !")
-        print("+ Nothing has been saved !")
-
-def lectureFic():
-    file = open('./diag/doc_diag17/diagrecap17.txt', 'r')
-    print(file.read())
-    file.close()
-    subprocess.call('./diag/doc_diag17/diag_read.py')
-
-def ajouterText():
-    textBox.delete('1.0', END)
-    textBox.insert(INSERT, "En date du : ")
-    textBox.insert(END, time.strftime("%d/%m/%Y à %H:%M:%S :") + '\n')
-    textBox.update()
-
-def importationFile(fichier, encodage="Utf-8"):
-    file = open(fichier, 'r', encoding=encodage)
-    content=file.readlines()
-    file.close()
-    for li in content:
-        textBox.insert(END, li)
 
 root=Tk()
 root.title("Diagnostics and ATCD")
@@ -84,8 +49,43 @@ entrytext.set(lineA1 + ', ' + lineA3 + ', ' + lineA5 + ', ' + lineA7)
 entryName=Entry(root, textvariable=entrytext, width=60)
 entryName.pack(padx=10, pady=10)
 
+def retrieve_input():
+    file = open('./diag/doc_diag17/diagrecap17.txt', 'a+')
+    file.write(textBox.get("1.0","end-1c") + "\n\n")
+    file.close()
+
+def messFromSafeButt():
+    MsgBox = messagebox.askquestion("Confirm","Are you sure ?\n"
+        "It will save all data !")
+    if MsgBox == 'yes':
+        retrieve_input()
+        textBox.insert(INSERT, "\n---Data saved !---")
+        print("+ Data saved !")
+    else:
+        textBox.insert(INSERT, "Nothing has been saved !")
+        print("+ Nothing has been saved !")
+
+def lectureFic():
+    file = open('./diag/doc_diag17/diagrecap17.txt', 'r')
+    print(file.read())
+    file.close()
+    subprocess.call('./diag/doc_diag17/diag_read.py')
+
+def ajouterText():
+    textBox.delete('1.0', END)
+    textBox.insert(INSERT, "En date du : ")
+    textBox.insert(END, time.strftime("%d/%m/%Y à %H:%M:%S :") + '\n')
+    textBox.update()
+
+def importationFile(fichier, encodage="Utf-8"):
+    file = open(fichier, 'r', encoding=encodage)
+    content=file.readlines()
+    file.close()
+    for li in content:
+        textBox.insert(END, li)
+
 textBox=Text(root, height=15, width=60, font=18, relief=SUNKEN)
-#textBox.insert(INSERT, "\nEn date du : ")
+#textBox.insert(INSERT, "En date du : ")
 #textBox.insert(END, time.strftime("%d/%m/%Y à %H:%M:%S :\n"))
 textBox.pack(padx=30, pady=30)
 
@@ -109,7 +109,13 @@ buttonQuitter=Button(root, text="Quit", fg='white', bg='navy',
     bd=3, highlightbackground='grey17', command=quit)
 buttonQuitter.pack(side='right', padx=10, pady=10)
 
-importationFile('./diag/doc_diag17/diagrecap17.txt',
-    encodage="Utf-8")
+try:
+    if os.path.getsize('./diag/doc_diag17/diagrecap17.txt'):
+        importationFile('./diag/doc_diag17/diagrecap17.txt', 
+            encodage="Utf-8")
+except FileNotFoundError as err_file:
+    print("+ File not found !", err_file)
+    messagebox.showwarning("WARNING", "File does not exist or " 
+        "file not found !")
 
 mainloop()
