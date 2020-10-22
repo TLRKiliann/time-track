48469f2935a20ed4288c69e02874a16d1567a1e1
TLRKiliann <philogenie@protonmail.com>
2020-10-22 23:24:21 +0200

    diag files and vmed files on work

--- vmed/doc_vmed20/vmed_read.py
+++ vmed/doc_vmed20/vmed_read.py
@@ -1,29 +1,12 @@
 #!/usr/bin/python3
-# -*-encoding:Utf-8-*-
+# -*- coding: utf-8 -*-
 
 
 from tkinter import *
+from tkinter import messagebox
+import os
+import subprocess
 
-
-def importationFile(fichier, encodage="Utf-8"):
-    with open(fichier, 'r', encoding=encodage) as filer:
-        content = filer.readlines()
-        for li in content:
-            textBox.insert(END, li)
-
-# To display name in Entry widget
-with open('./newpatient/entryfile20.txt', 'r') as filename:
-    line1=filename.readline()
-
-# To display allergy in Entry widget
-with open('./allergy/allergyfile20.txt', 'r') as allerfile:
-    lineA1=allerfile.readline()
-    lineA2=allerfile.readline()
-    lineA3=allerfile.readline()
-    lineA4=allerfile.readline()
-    lineA5=allerfile.readline()
-    lineA6=allerfile.readline()
-    lineA7=allerfile.readline()
 
 fen=Tk()
 fen.title("Results of Medical Visit")
@@ -39,6 +22,10 @@ labelo=Label(fen, text="Results of Medical Visit for : ",
     font='Arial 18 bold', fg='navy', bg='cyan')
 labelo.pack(in_=top, side=LEFT, padx=5, pady=20)
 
+# To display name in Entry widget
+with open('./newpatient/entryfile20.txt', 'r') as filename:
+    line1=filename.readline()
+
 text_name=StringVar()
 text_name.set(line1)
 Entryname=Entry(fen, textvariable=text_name)
@@ -48,10 +35,26 @@ labelallergy=Label(fen, text="Allergy",
     font='Arial 18 bold', fg='coral', bg='cyan')
 labelallergy.pack(padx=5, pady=5)
 
+# To display allergy in Entry widget
+with open('./allergy/allergyfile20.txt', 'r') as allerfile:
+    lineA1=allerfile.readline()
+    lineA2=allerfile.readline()
+    lineA3=allerfile.readline()
+    lineA4=allerfile.readline()
+    lineA5=allerfile.readline()
+    lineA6=allerfile.readline()
+    lineA7=allerfile.readline()
+
 text_aller=StringVar()
 text_aller.set(lineA1 + ', ' + lineA3 + ', ' + lineA5 + ', ' + lineA7)
 Entryall=Entry(fen, textvariable=text_aller, width=60)
 Entryall.pack(padx=10, pady=5)
+
+def importationFile(fichier, encodage="Utf-8"):
+    with open(fichier, 'r', encoding=encodage) as filer:
+        content = filer.readlines()
+        for li in content:
+            textBox.insert(END, li)
 
 textBox=Text(fen, height=15, width=60, font=18, relief=SUNKEN)
 textBox.pack(padx=30, pady=30)
@@ -61,7 +64,13 @@ buttonClose=Button(fen, text="Quit", width=8, bd=3,
     activebackground='dark turquoise', command=quit)
 buttonClose.pack(side='right', padx=10, pady=10)
 
-importationFile('./vmed/doc_vmed20/resultvmed.txt',
-    encodage="Utf-8")
+try:
+    if os.path.getsize('./vmed/doc_vmed20/resultvmed20.txt'):
+        importationFile('./vmed/doc_vmed20/resultvmed20.txt',
+            encodage="Utf-8")
+except FileNotFoundError as err_file:
+    print("+ File not found !", err_file)
+    messagebox.showwarning("WARNING", "File does not exist or "
+        "file not found !")
 
 fen.mainloop()
